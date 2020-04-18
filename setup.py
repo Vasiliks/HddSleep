61d619988669e8e6c17023d8724b581f65caec4d
Vasiliks <49211115+Vasiliks@users.noreply.github.com>
2020-04-18 09:11:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name = 'enigma2-plugin-extensions-hddsleep',
 		version='1.72',
 		packages=['Extensions.HddSleep'],
 		package_dir = {'Extensions.HddSleep': 'src'},
-		package_data={'Extensions.HddSleep': ['*.png', 'hddsleep.sh', 'keymap.xml', 'bin/*']},
+		package_data={'Extensions.HddSleep': ['*.png', 'hddsleep.sh', 'keymap.xml', 'bin/*/*']},
 		description = 'Power management for harddisks',
 		cmdclass = setup_translate.cmdclass,
 	)
